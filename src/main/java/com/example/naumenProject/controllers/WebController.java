f6b81886ece7b8b333f2922ca75c0cd6fa12a1cb
FUFSoB <fufsob@fufsob.ru>
2024-04-25 17:42:37 +0500

    base info on index

--- src/main/java/com/example/naumenProject/controllers/WebController.java
+++ src/main/java/com/example/naumenProject/controllers/WebController.java
@@ -1,14 +1,35 @@
 package com.example.naumenProject.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.naumenProject.services.ProjectService;
+
+import org.springframework.ui.Model;
+import com.example.naumenProject.models.User;
+import com.example.naumenProject.repositories.UserRepository;
+
+import org.springframework.security.core.Authentication;
 
 @Controller
 public class WebController
 {
+    private final UserRepository userRepository;
 
+    @Autowired
+    public WebController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
+    @GetMapping(value = "/")
+    public String getMainPage(Model model, Authentication authentication) {
+        String username = authentication.getName();
+
+        User currentUser = userRepository.findUserByUsername(username);
+
+        model.addAttribute("user", currentUser);
+
+        return "index";
+    }
 }
