cbc20b68fe00f68903a527af5c13a82db5a924da
FUFSoB <fufsob@fufsob.ru>
2024-06-01 13:58:47 +0500

    basic uploads

--- src/main/java/com/example/naumenProject/controllers/WebController.java
+++ src/main/java/com/example/naumenProject/controllers/WebController.java
@@ -1,15 +1,20 @@
 package com.example.naumenProject.controllers;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.naumenProject.models.Project;
 import com.example.naumenProject.models.User;
@@ -88,6 +93,44 @@ public class WebController {
         projectService.deleteProject(id);
 
         return "redirect:/projects";
+    }
+
+
+    @PostMapping(value = "/uploadArchive")
+    public ResponseEntity<String> uploadArchive(@RequestParam("id") Long id, @RequestParam("file") MultipartFile archiveFile,
+            Authentication authentication) {
+        var project = projectService.getProjectById(id);
+
+        if (project != null && !archiveFile.isEmpty()) {
+            String contentType = archiveFile.getContentType();
+            if (!"application/zip".equals(contentType) && !"application/x-zip-compressed".equals(contentType)) {
+                return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body("Only zip files are allowed.");
+            }
+
+            String uploadDir = System.getProperty("user.dir") + File.separator + "uploads" + File.separator + String.valueOf(id) + File.separator;
+            String originalFilename = archiveFile.getOriginalFilename();
+            String filePath = uploadDir + originalFilename;
+
+            try {
+                File directory = new File(uploadDir);
+                if (!directory.exists()) {
+                    directory.mkdirs();
+                }
+
+                File file = new File(filePath);
+                archiveFile.transferTo(file);
+
+                project.setProjectArchivePath(filePath);
+                projectService.updateProject(project);
+
+                return ResponseEntity.status(HttpStatus.OK).body("File uploaded successfully.");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading file.");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid project ID or empty file.");
+        }
     }
 
     @GetMapping(value = "/ratings")
